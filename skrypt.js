b8bda9be3faf12254d28196d3fdbb281d1be9412
Sajmon444 <113638328+Sajmon444@users.noreply.github.com>
2023-04-06 23:12:00 +0200

    skończone
    można jeszcze kiedys w przyszłości rozbudować

--- skrypt.js
+++ skrypt.js
@@ -37,16 +37,20 @@ for (let i = 0; i < elementy.length; i++) {
 
 //rysowanie wrogow
 
-
 var pozycja_wroga = Math.floor(Math.random() * 6) + 80;  //pozycja wroga ana samym poczatku
 tab[pozycja_wroga].classList.add('wrog')
 
 
 
+
+
+
 //rysowanie gracz 
 var pozycja_gracz = Math.floor(Math.random() * 178) + 1;  //pozycja gracz ana samym poczatku
-
 tab[pozycja_gracz].classList.add('gracz')
+
+
+
 
 //dodanie poruszania sie gracza na prawo i lewo za pomoca a i d   // od 150 do 164
 document.addEventListener("keydown", prawo)
@@ -62,8 +66,19 @@ function prawo(event) {
         if (pozycja_gracz !== 14 && pozycja_gracz !== 29 && pozycja_gracz !== 44 && pozycja_gracz !== 59 && pozycja_gracz !== 74 && pozycja_gracz !== 89 && pozycja_gracz !== 104 && pozycja_gracz !== 119 && pozycja_gracz !== 134 && pozycja_gracz !== 149 && pozycja_gracz !== 164 && pozycja_gracz !== 179) {
             tab[pozycja_gracz].classList.remove('gracz')
             pozycja_gracz++
-            tab[pozycja_gracz].classList.add('gracz')
-            console.log("pozycja gracza to:", pozycja_gracz)
+            if (pozycja_gracz !== pozycja_wroga) {
+
+                tab[pozycja_gracz].classList.add('gracz')
+                console.log("pozycja gracza to:", pozycja_gracz)
+
+            } else {
+                tab[pozycja_gracz].classList.remove('gracz')
+                tab[pozycja_wroga].classList.remove('wrog')
+                clearInterval(powtarzanie_wroga);
+                
+                
+                alert("przegrales ")
+            }
         } else {
             console.log('nie mozesz dalej pojsc')
         }
@@ -77,8 +92,19 @@ function lewo(event) {
         if (pozycja_gracz !== 0 && pozycja_gracz !== 15 && pozycja_gracz !== 30 && pozycja_gracz !== 45 && pozycja_gracz !== 60 && pozycja_gracz !== 75 && pozycja_gracz !== 90 && pozycja_gracz !== 105 && pozycja_gracz !== 120 && pozycja_gracz !== 135 && pozycja_gracz !== 150 && pozycja_gracz !== 165) {
             tab[pozycja_gracz].classList.remove('gracz')
             pozycja_gracz--
-            tab[pozycja_gracz].classList.add('gracz')
-            console.log("pozycja gracza to:", pozycja_gracz)
+            if (pozycja_gracz !== pozycja_wroga) {
+
+                tab[pozycja_gracz].classList.add('gracz')
+                console.log("pozycja gracza to:", pozycja_gracz)
+
+            } else {
+                tab[pozycja_gracz].classList.remove('gracz')
+                tab[pozycja_wroga].classList.remove('wrog')
+                clearInterval(powtarzanie_wroga);
+                
+                
+                alert("przegrales ")
+            }
         } else {
             console.log('nie mozesz dalej pojsc')
         }
@@ -95,9 +121,20 @@ function gora(event) {
 
             tab[pozycja_gracz].classList.remove('gracz')
             pozycja_gracz -= 15
-            if (pozycja_gracz !== pozycja_wroga)
+
+            if (pozycja_gracz !== pozycja_wroga) {
                 tab[pozycja_gracz].classList.add('gracz')
-            console.log("pozycja gracza to:", pozycja_gracz)
+
+                console.log("pozycja gracza to:", pozycja_gracz)
+            } else {
+                tab[pozycja_gracz].classList.remove('gracz')
+                tab[pozycja_wroga].classList.remove('wrog')
+                clearInterval(powtarzanie_wroga);
+                
+                
+                alert("przegrales ")                                           //DOKONCZYC TEN ELEMENT I ZROBIC TO W INNYCH
+            }
+
         } else {
             console.log('nie mozesz dalej pojsc')
         }
@@ -113,8 +150,19 @@ function dol(event) {
         if (pozycja_gracz != 165 && pozycja_gracz != 166 && pozycja_gracz !== 167 && pozycja_gracz !== 168 && pozycja_gracz !== 169 && pozycja_gracz !== 170 && pozycja_gracz !== 171 && pozycja_gracz !== 172 && pozycja_gracz !== 173 && pozycja_gracz !== 174 && pozycja_gracz !== 175 && pozycja_gracz !== 176 && pozycja_gracz !== 177 && pozycja_gracz !== 178 && pozycja_gracz !== 179) {
             tab[pozycja_gracz].classList.remove('gracz')
             pozycja_gracz = pozycja_gracz + 15
-            tab[pozycja_gracz].classList.add('gracz')
-            console.log("pozycja gracza to:", pozycja_gracz)
+
+            if (pozycja_gracz !== pozycja_wroga) {
+
+                tab[pozycja_gracz].classList.add('gracz')
+                console.log("pozycja gracza to:", pozycja_gracz)
+
+            } else {
+                tab[pozycja_gracz].classList.remove('gracz')
+                tab[pozycja_wroga].classList.remove('wrog')
+                clearInterval(powtarzanie_wroga);
+               
+                alert("przegrales ")
+            }
         }
         else {
             console.log('nie mozesz dalej pojsc')
@@ -189,13 +237,211 @@ function poruszanie_wroga() {
 }
 
 
-var powtarzanie_wroga = setInterval(poruszanie_wroga, 150);
+var powtarzanie_wroga = setInterval(poruszanie_wroga, 300);
+
+
+//STRZELANIE POCISKAMI PRZEZ WROGA
+
+
+function generujLosowaLiczbe() {
+    return Math.floor(Math.random() * 4) + 1;
+}
+
+setInterval(() => {
+    var losowanie_pocisk = generujLosowaLiczbe();
+    console.log("wygenerowana losowa liczba pocisku: ", losowanie_pocisk);
 
 
 
 
 
+    var pozycja_pocisku_wroga
 
+    let isBulletOnScreen_wrog = false;
+
+    //poruszanie pocisku wroga w gore
+    if (losowanie_pocisk == 1 && !isBulletOnScreen_wrog) {
+        isBulletOnScreen_wrog = true;
+        pozycja_pocisku_wroga = pozycja_wroga - 15;
+        console.log("pocisk pojawia sie pocisk wroga: ", pozycja_pocisku_wroga);
+        tab[pozycja_pocisku_wroga].classList.add("pocisk_wroga");
+
+        let intervalId_wrog = setInterval(function () {
+            if (pozycja_pocisku_wroga > 15) {
+                // Sprawdzanie kolizji pomiędzy pociskiem a wrogiem
+                if (pozycja_pocisku_wroga === pozycja_gracz) {
+                    console.log("Trafiony");
+
+                    clearInterval(intervalId_wrog);
+                    tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+
+                    clearInterval(powtarzanie_wroga);
+                    tab[pozycja_wroga].classList.remove('wrog')
+
+                    tab[pozycja_gracz].classList.remove('gracz')
+
+                    isBulletOnScreen_wrog = false;
+
+                    alert('przegrales trafil cie pocisk')
+                }else if(pozycja_pocisku==pozycja_pocisku_wroga){
+                    tab[pozycja_pocisku].classList.remove('pocisk')
+                    tab[pozycja_pocisku_wroga].classList.remove('pocisk_wroga')
+                
+                
+                 }else {
+                    tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+                    pozycja_pocisku_wroga -= 15;
+                    console.log("pozycja pocisku wroga: ", pozycja_pocisku_wroga);
+                    tab[pozycja_pocisku_wroga].classList.add("pocisk_wroga");
+                }
+            } else {
+                console.log("pocisk wroga nie moze isc dalej");
+                clearInterval(intervalId_wrog);
+                tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+                isBulletOnScreen_wrog = false;
+            }
+        }, 200);
+    }
+
+
+
+    //poruszanie pocisku wroga w dol
+    if (losowanie_pocisk == 2 && !isBulletOnScreen_wrog) {
+        isBulletOnScreen_wrog = true;
+        pozycja_pocisku_wroga = pozycja_wroga + 15;
+        console.log("pocisk pojawia sie: ", pozycja_pocisku_wroga);
+        tab[pozycja_pocisku_wroga].classList.add("pocisk_wroga");
+
+        let intervalId_wrog = setInterval(function () {
+            if (pozycja_pocisku_wroga < 165) {
+                // Sprawdzanie kolizji pomiędzy pociskiem a wrogiem
+                if (pozycja_pocisku_wroga === pozycja_gracz) {
+                    console.log("Trafiony");
+
+                    clearInterval(intervalId_wrog);
+                    tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+
+                    clearInterval(powtarzanie_wroga);
+                    tab[pozycja_wroga].classList.remove('wrog')
+
+                    tab[pozycja_gracz].classList.remove('gracz')
+
+                    isBulletOnScreen_wrog = false;
+
+                    alert('przegrales trafil cie pocisk')
+                }else if(pozycja_pocisku==pozycja_pocisku_wroga){
+                    tab[pozycja_pocisku].classList.remove('pocisk')
+                    tab[pozycja_pocisku_wroga].classList.remove('pocisk_wroga')
+                
+                
+                 } else {
+                    tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+                    pozycja_pocisku_wroga += 15;
+                    console.log("pozycja pocisku: ", pozycja_pocisku_wroga);
+                    tab[pozycja_pocisku_wroga].classList.add("pocisk_wroga");
+                }
+            } else {
+                console.log("pocisk wroga nie moze isc dalej");
+                clearInterval(intervalId_wrog);
+                tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+                isBulletOnScreen_wrog = false;
+            }
+        }, 200);
+    }
+
+
+    //poruszanie pocisku wroga w lewo
+    if (losowanie_pocisk == 3 && !isBulletOnScreen_wrog) {
+        isBulletOnScreen_wrog = true;
+        pozycja_pocisku_wroga = pozycja_wroga - 1;
+        console.log("pocisk wroga pojawia sie: ", pozycja_pocisku_wroga);
+        tab[pozycja_pocisku_wroga].classList.add("pocisk_wroga");
+
+        let intervalId_wrog = setInterval(function () {
+            if (pozycja_pocisku_wroga !== 0 && pozycja_pocisku_wroga !== 15 && pozycja_pocisku_wroga !== 30 && pozycja_pocisku_wroga !== 45 && pozycja_pocisku_wroga !== 60 && pozycja_pocisku_wroga !== 75 && pozycja_pocisku_wroga !== 90 && pozycja_pocisku_wroga !== 105 && pozycja_pocisku_wroga !== 120 && pozycja_pocisku_wroga !== 135 && pozycja_pocisku_wroga !== 150 && pozycja_pocisku_wroga !== 165) {
+                // Sprawdzanie kolizji pomiędzy pociskiem a wrogiem
+                if (pozycja_pocisku_wroga === pozycja_gracz) {
+                    console.log("Trafiony");
+
+                    clearInterval(intervalId_wrog);
+                    tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+
+                    clearInterval(powtarzanie_wroga);
+                    tab[pozycja_wroga].classList.remove('wrog')
+
+                    tab[pozycja_gracz].classList.remove('gracz')
+
+                    isBulletOnScreen_wrog = false;
+
+                    alert('przegrales trafil cie pocisk')
+                }else if(pozycja_pocisku==pozycja_pocisku_wroga){
+                    tab[pozycja_pocisku].classList.remove('pocisk')
+                    tab[pozycja_pocisku_wroga].classList.remove('pocisk_wroga')
+                
+                
+                 } else {
+                    tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+                    pozycja_pocisku_wroga -= 1;
+                    console.log("pozycja pocisku wroga: ", pozycja_pocisku_wroga);
+                    tab[pozycja_pocisku_wroga].classList.add("pocisk_wroga");
+                }
+            } else {
+                console.log("pocisk wroga nie moze isc dalej");
+                clearInterval(intervalId_wrog);
+                tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+                isBulletOnScreen_wrog = false;
+            }
+        }, 200);
+    }
+
+
+    //poruszanie pocisku wroga w prawo
+    if (losowanie_pocisk == 4 && !isBulletOnScreen_wrog) {
+        isBulletOnScreen_wrog = true;
+        pozycja_pocisku_wroga = pozycja_wroga + 1;
+        console.log("pocisk wroga pojawia sie: ", pozycja_pocisku_wroga);
+        tab[pozycja_pocisku_wroga].classList.add("pocisk_wroga");
+
+        let intervalId_wrog = setInterval(function () {
+            if (pozycja_pocisku_wroga !== 14 && pozycja_pocisku_wroga !== 29 && pozycja_pocisku_wroga !== 44 && pozycja_pocisku_wroga !== 59 && pozycja_pocisku_wroga !== 74 && pozycja_pocisku_wroga !== 89 && pozycja_pocisku_wroga !== 104 && pozycja_pocisku_wroga !== 119 && pozycja_pocisku_wroga !== 134 && pozycja_pocisku_wroga !== 149 && pozycja_pocisku_wroga !== 164 && pozycja_pocisku_wroga !== 179) {
+                // Sprawdzanie kolizji pomiędzy pociskiem a wrogiem
+                if (pozycja_pocisku_wroga === pozycja_gracz) {
+                    console.log("Trafiony");
+
+                    clearInterval(intervalId_wrog);
+                    tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+
+                    clearInterval(powtarzanie_wroga);
+                    tab[pozycja_wroga].classList.remove('wrog')
+
+                    tab[pozycja_gracz].classList.remove('gracz')
+
+                    isBulletOnScreen_wrog = false;
+
+                    alert('przegrales trafil cie pocisk')
+                }else if(pozycja_pocisku==pozycja_pocisku_wroga){
+                    tab[pozycja_pocisku].classList.remove('pocisk')
+                    tab[pozycja_pocisku_wroga].classList.remove('pocisk_wroga')
+                
+                
+                 } else {
+                    tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+                    pozycja_pocisku_wroga += 1;
+                    console.log("pozycja pocisku wroga: ", pozycja_pocisku_wroga);
+                    tab[pozycja_pocisku_wroga].classList.add("pocisk_wroga");
+                }
+            } else {
+                console.log("pocisk wroga nie moze isc dalej");
+                clearInterval(intervalId_wrog);
+                tab[pozycja_pocisku_wroga].classList.remove("pocisk_wroga");
+                isBulletOnScreen_wrog = false;
+            }
+        }, 200);
+    }
+
+
+
+}, 1000);
 //strzelanie pociskiem tylko pojawianie sie pocisku   i poruszanie pocisku       i
 //                                                                             j k l
 /*
@@ -379,7 +625,7 @@ function dodajPunkt() {
 
 function generuj_wroga() {
     tab[pozycja_wroga].classList.add('wrog');
-    powtarzanie_wroga = setInterval(poruszanie_wroga, 150)
+    powtarzanie_wroga = setInterval(poruszanie_wroga, 300)
     poruszanie_wroga()
 }
 
